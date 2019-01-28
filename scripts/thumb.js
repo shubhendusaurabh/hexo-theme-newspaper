80fb1d7c811752d4ff33ae3708e491eb5d46efbc
shubhu <shubhendu.saurabh@hotmail.com>
2019-01-28 12:40:37 +0530

    thumb err fix

--- scripts/thumb.js
+++ scripts/thumb.js
@@ -22,12 +22,12 @@ hexo.extend.helper.register('thumb', function (path) {
   // console.log(filename, path)
   // yes then return path
   fs.access(`${hexo.source_dir}${filename}`, (err) => {
-    if (!err) {
+    if (err) {
       return filename;
     }
     // no then generate new thumb with sharp
     // console.log(`${hexo.source_dir}${filename}`)
-    resize = spawn('convert', ['-resize', '480x240', `${hexo.source_dir}${path}`, `${hexo.source_dir}${filename}`])
+    const resize = spawn('convert', ['-resize', '480x240', `${hexo.source_dir}${path}`, `${hexo.source_dir}${filename}`])
     
     resize.stdout.on('data', data => {
       console.log(`stdout: ${data}`);
