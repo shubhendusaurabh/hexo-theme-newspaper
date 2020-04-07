ec1bfdbb8013133e472ee886d7e873b8592860f8
shubhu <shubhendu.saurabh@hotmail.com>
2020-04-07 22:28:48 +0530

    bug fixes

--- scripts/button.js
+++ scripts/button.js
@@ -8,7 +8,7 @@ hexo.extend.tag.register('button', function (args, content) {
   let link = args.find((arg) => arg.indexOf('link') > -1)
   let href = 'https://www.amazon.com';
   if (link.length) {
-    href = link.split('=')[1]
+    href = link.split('link=')[1]
   }
   return `<div class="tc"><a class="f4 grow no-underline ba ph3 pv2 mb2 dib amzn-btn" href="${href}" target="_blank">${content}</a></div>`
 }, {ends: true});
